e3b9af044c1d4dbef60f636493b561f21d924e52
cha80537 <cha80537@gmail.com>
2024-09-27 18:42:38 +0900

    v0.3 io & types

--- week05/main.go
+++ week05/main.go
@@ -1,18 +1,29 @@
 package main
 
-import "fmt" // c언어의 #include <stdio.h>
+import (
+	"fmt" // c언어의 #include <stdio.h>
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
 	//var i int
 	//i = 55
 
 	//var i int = 55
-	var f float32 = 1.92
 
-	i := 55
+	var f float64 = 1.92
+
+	//f := 1.92
+	i := "55"
+	fmt.Println((strings.Title("kim inha")))
+	fmt.Printf("%f %f\n", f, math.Ceil(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 	fmt.Println("f is", f)
 	fmt.Println("i is", i)
 	fmt.Print("i is ", i, "\n")
 	fmt.Println("i is", i)
-	fmt.Printf("i is %d\n", i)
+	//fmt.Printf("i is %d\n", i)
+	fmt.Printf("i is %s\n", i)
 }
